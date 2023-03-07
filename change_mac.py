54e2a738d5ff7dc0c3a8efca1cacaa44582147d3
Alexander <70129908+bigfudgge@users.noreply.github.com>
2023-03-07 12:57:43 +0500

    Update change_mac.py

--- change_mac.py
+++ change_mac.py
@@ -6,29 +6,33 @@ import os
 
 
 #Use ifconfig to display information about the state of active network interfaces
-ifconfig = subprocess.run(['ifconfig'])
-
-print(ifconfig)
+subprocess.run(['ifconfig', '-a'])
 
 
 #Add the name of the network interface to the variable
 interface = input("Введите имя сетевого интерфейса: ")
 
+original_conf = subprocess.check_output(['ifconfig', interface])
+
 factory_mac = subprocess.check_output(['ethtool', '-P', interface]).decode().split(" ")[-1].strip()
 
-print(f"Текущий MAC-адрес: {factory_mac}")
+print(f"\n{'='*10}Текущий MAC-адрес: {factory_mac}")
 
 
-#Function for changing mac address
+
+#function for changing mac
 def change_mac():
-    mac_list = ['b1:2c:dd:e6:63:fg', 'b1:2c:dd:e6:63:fd', 'b1:2c:dd:e6:63:re', 'b1:2c:dd:e6:63:cc']
+    mac_list = ['mac1', 'mac2', 'mac3', 'mac4']
 
-    print("Доступные MAC-адреса: [1] TSV, [2] ARM, [3] WiFi, [4] Random:")
+    print("\nДоступные MAC-адреса:")
+    print("1. TSV"),
+    print("2. ARM_KIOSK")
+    print("3. WiFi")
+    print("4. Случайный")
+    #for i, mac in enumerate(mac_list):
+        #print(f"{i+1}: {mac}")
 
-    for i, mac in enumerate(mac_list):
-        print(f"{i+1}: {mac}")
-
-    mac_choice = int(input("Выберите номер MAC-адреса: "))
+    mac_choice = int(input("\nВыберите номер MAC-адреса: "))
     mac_address = mac_list[mac_choice-1]
 
     command_down = f'sudo ifconfig {interface} down'
@@ -37,7 +41,7 @@ def change_mac():
     subprocess.call(command_hw, shell=True)
     command_up = f'sudo ifconfig {interface} up'
     subprocess.call(command_up, shell=True)
-    print(f"MAC-адрес изменен на {mac_address}")
+    print(f"\n{'='*10}MAC-адрес изменен на {mac_address}")
 
 #Function to return the mac address
 def return_mac():
@@ -46,19 +50,41 @@ def return_mac():
         subprocess.call(['sudo', 'ifconfig', interface, 'down'])
         subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', factory_mac])
         subprocess.call(['sudo', 'ifconfig', interface, 'up'])
-        print(f"MAC-адрес вернулся на заводской: {factory_mac}")
+        print(f"\n{'='*10}MAC-адрес вернулся на заводской: {factory_mac}")
 
     else:
-        print("MAC-адрес оставлен без изменений")
+        print("\nMAC-адрес оставлен без изменений")
+
+
+
+#changing IP
+def change_ip():
+    new_ip = input("\nВведите новый IP-адрес: ")
+    new_netmask = input("\nВведите новую маску подсети: ")
+
+    subprocess.run(['sudo', 'ifconfig', interface, 'down'])
+    subprocess.run(['sudo', 'ifconfig', interface, new_ip, 'netmask', new_netmask])
+    subprocess.run(['sudo', 'ifconfig', interface, 'up'])
+    print("\nIP-адрес и маска подсети изменены:")
+    print(subprocess.check_output(['ifconfig', interface]))
+
+
 
 #Cycle for selecting actions
 while True:
-    action = input("Выберите действие (1 - смена mac-адреса), (2 - вернуть заводской mac-адрес) (3 - выйти): ")
+    print("\nВыберите действие:")
+    print("1. Сменить MAC-адрес")
+    print("2. Вернуть заводской MAC")
+    print("3. Сменить IP-адрес и маску подсети")
+    print("4. Выйти")
+    action = input("\nВыберите номер действия: ")
     if action == "1":
         change_mac()
     elif action == "2":
         return_mac()
     elif action == "3":
+        change_ip()
+    elif action == "4":
         break
     else:
-        print("Некорректный выбор!")
+        print("Некорректный выбор действия!")
